974977ac28c91cc133205b63791c65310bd0f4f6
Bruno Costa <bruno.sc@outlook.com.br>
2018-07-20 23:28:42 -0300

    Initial commit

---
+++ src/main/java/com/eletroinfo/eletroinfo/comparator/EletroinfoComparatorApplication.java
@@ -0,0 +1,12 @@
+package com.eletroinfo.eletroinfo.comparator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EletroinfoComparatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EletroinfoComparatorApplication.class, args);
+	}
+}
